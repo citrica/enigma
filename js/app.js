eb94888ee9a05e4630ce7e249e8e1d00ee1e6a33
Inma Rueda <inma.rueda28@gmail.com>
2021-10-03 17:46:53 +0200

    v2 ejercicio generador aleatorio por consola

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // Ejercicio encriptar
 
-var plainAlphabet1 = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
-var encryptedAlphabet1 = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
-var text = "b': yqg ,:agr(hue:) shrerg jq,u'qf q !hg ,:psqñrf:g rg,fuwurae: s:f r' ,yq!x mbg:z 'q 'r,yrn !hfua( t'usqfuq ,:apu(:)m z q ,:a!uahq,u:a rajuq ha pragqir ,utfqe:n br' dhr !h dhurfqg)";
-var text2 = "Hola mundo";
+var plainAlphabet1 = "aom";
+var encryptedAlphabet1 = "sz*";
+var plainAlphabet2 = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+var encryptedAlphabet2 = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+var text1 = "hola amigo";
+var text2 = "b': yqg ,:agr(hue:) shrerg jq,u'qf q !hg ,:psqñrf:g rg,fuwurae: s:f r' ,yq!x mbg:z 'q 'r,yrn !hfua( t'usqfuq ,:apu(:)m z q ,:a!uahq,u:a rajuq ha pragqir ,utfqe:n br' dhr !h dhurfqg)";
+
 
 var searchIndex = (letter, alphabet) => {
     return alphabet.indexOf(letter);
@@ -24,24 +27,41 @@ var transformText = (text, origin, destination) => {
     return newText;
 }
 
-console.log("Texto introducido: " + text2);
-console.log("Texto encriptado: " + transformText(text2, plainAlphabet1, encryptedAlphabet1));
-console.log("Texto introducido: " + text);
-console.log("Texto desencriptado: " + transformText(text, encryptedAlphabet1, plainAlphabet1));
+// console.log("Texto introducido: " + text1);
+// console.log("Texto encriptado: " + transformText(text1, plainAlphabet1, encryptedAlphabet1));
+// console.log("Texto introducido: " + text2);
+// console.log("Texto desencriptado: " + transformText(text2, encryptedAlphabet2, plainAlphabet2));
 
 // Ejecicio generador aleatorio
 
+var randomNumber = (range, min) => Math.floor(Math.random() * range) + min;
+
+var notRepeatNumber = (index, array, range, min) => {
+    var j = index;
+    while (0 < j) {
+        if (array[index] === array[j - 1]) {
+            resultNew = randomNumber(range, min);
+            array[index] = resultNew;
+            j = index;
+        } else {
+            j--;
+        }
+    }
+    return array[index];
+}
+
 var randomPick = (n, min, max) => {
     var range = max - min + 1;
-    var result = [];
+    var result = new Array(n);
+
     for (var i = 0; i < n; i++) {
-        result[i] = Math.floor(Math.random() * range) + min;
-        //No deben repetirse los numeros escogidos aleatoriamente
+        result[i] = randomNumber(range, min);
+        notRepeatNumber(i, result, range, min);
     }
     return result;
 }
 
 console.log(randomPick(10, 1, 100));
-// randomPick(6, 1, 49); // Apuesta automática de la primitiva
-// randomPick(15, 1, 15); // Escoge combinación de bolas de billar
-// randomPick(1, 1, 6); // Tirada aleatoria de un dado
+console.log(randomPick(6, 1, 49)); // Apuesta automática de la primitiva
+console.log(randomPick(15, 1, 15)); // Escoge combinación de bolas de billar
+console.log(randomPick(1, 1, 6)); // Tirada aleatoria de un dado
